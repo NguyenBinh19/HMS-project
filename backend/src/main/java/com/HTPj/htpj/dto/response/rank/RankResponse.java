9e45c8efd8a61f34c063b1ce0095a085bc0b7d45
tranghh <huyentranghoang1111@gmail.com>
2026-03-23 20:19:24 +0700

    set rank basic for agency

--- backend/src/main/java/com/HTPj/htpj/dto/response/rank/RankResponse.java
+++ backend/src/main/java/com/HTPj/htpj/dto/response/rank/RankResponse.java
@@ -12,6 +12,7 @@ import java.math.BigDecimal;
 public class RankResponse {
 
     private Integer id;
+    private String rankCode;
     private String rankName;
     private String description;
     private String icon;
--- backend/src/main/java/com/HTPj/htpj/entity/Rank.java
+++ backend/src/main/java/com/HTPj/htpj/entity/Rank.java
@@ -14,43 +14,64 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Rank {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "rank_name")
     private String rankName;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "priority", nullable = false)
     private Integer priority;
 
+    @Column(name = "is_active")
     private Boolean isActive;
 
+    @Column(name = "min_total_booking")
     private Integer minTotalBooking;
 
+    @Column(name = "min_total_revenue")
     private BigDecimal minTotalRevenue;
 
+    @Column(name = "logic")
     private String logic;
 
+    @Column(name = "maintain_min_booking")
     private Integer maintainMinBooking;
 
+    @Column(name = "maintain_min_revenue")
     private BigDecimal maintainMinRevenue;
 
+    @Column(name = "maintain_logic")
     private String maintainLogic;
 
+    @Column(name = "credit_limit")
     private BigDecimal creditLimit;
 
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Column(name = "created_by")
     private String createdBy;
 
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Column(name = "updated_by")
     private String updatedBy;
 
+    @Column(name = "color")
     private String color;
 
+    @Column(name = "icon")
     private String icon;
+
+    @Column(name = "rank_code", nullable = false, unique = true)
+    private String rankCode;
 
     @OneToMany(mappedBy = "rank")
     private List<Agency> agencies;
