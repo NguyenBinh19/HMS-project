4f8698a6fa6f8ff6588bfd891d1fd4d5b3f40beb
ThanhKH <khuathanh20042@gmail.com>
2026-04-21 15:11:57 +0700

    merge rank-commision and revenue-allotment

--- backend/src/main/java/com/HTPj/htpj/controller/AIController.java
+++ backend/src/main/java/com/HTPj/htpj/controller/AIController.java
@@ -1,28 +1,28 @@
-//package com.HTPj.htpj.controller;
-//
-//import com.HTPj.htpj.service.AIService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/ai")
-//@RequiredArgsConstructor
-//public class AIController {
-//
-//    private final AIService aiService;
-//
-//    @PostMapping("/chat")
-//    public Map<String, String> chat(@RequestBody Map<String, String> req) {
-//
-//        String message = req.get("message");
-//
-//        String response = aiService.askAI(message);
-//
-//        return Map.of("reply", response);
-//    }
-//}
+package com.HTPj.htpj.controller;
+
+import com.HTPj.htpj.service.AIService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ai")
+@RequiredArgsConstructor
+public class AIController {
+
+    private final AIService aiService;
+
+    @PostMapping("/chat")
+    public Map<String, String> chat(@RequestBody Map<String, String> req) {
+
+        String message = req.get("message");
+
+        String response = aiService.askAI(message);
+
+        return Map.of("reply", response);
+    }
+}
