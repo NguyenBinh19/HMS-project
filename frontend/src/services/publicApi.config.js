febf742525dc05fdd76319a0f341fe9a690d5cb3
Nguyễn Bình <binhco2004@gmail.com>
2026-04-24 14:51:45 +0700

    fix: login with gg

--- frontend/src/services/publicApi.config.js
+++ frontend/src/services/publicApi.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-    import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:8080/hms";
+    import.meta.env.VITE_REACT_APP_API_URL || "http://localhost:8080";
 
 const publicApi = axios.create({
     baseURL: API_URL,
